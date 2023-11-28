46ea050e86d4dc9d66033132c3eea3f428b1eec8
ZenGB_07 <nguyenngocgiabao0701@gmail.com>
2023-11-28 20:02:46 +0700

    Add Animator

--- Assets/Spricts/Movement.cs
+++ Assets/Spricts/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
-
+    public Animator animator;
 
     // get input from player
     //apply in sprites
@@ -19,8 +19,30 @@ public class Movement : MonoBehaviour
         //set vector with variable
         Vector3 direction = new Vector3(horizontal, vertical);
 
+        //set up animation
+        AnimateMovement(direction);
+
         //set vector to tranform position
         transform.position += direction * speed * Time.deltaTime;
         
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if (animator != null)
+        {
+            if (direction.magnitude > 0)
+            {
+                //isMoving in Anmatior true
+                animator.SetBool("isMoving", true);
+                //set animation with up, down, left, right
+                animator.SetFloat("Horizontal", direction.x);
+                animator.SetFloat("Verticla", direction.y);
+            }
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
 }
